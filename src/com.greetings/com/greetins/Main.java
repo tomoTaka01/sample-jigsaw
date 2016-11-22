cbf828e6188d0518a09c88b0fd2bb1968b3ee82d
tomo taka <tomo_taka01@hotmail.com>
2016-11-22 23:16:14 +0900

    ADD some files

--- src/com.greetings/com/greetins/Main.java
+++ src/com.greetings/com/greetins/Main.java
@@ -1,6 +1,7 @@
 package com.greetings;
+import org.astro.World;
 public class Main {
     public static void main(String... args) {
-        System.out.println("Greetings!");
+        System.out.println(String.format("Greeting %s", World.name()));
     }
 }
